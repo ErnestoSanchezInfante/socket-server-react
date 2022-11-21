55c5641b7154f4b99cb73a67bce01eb1cecaedde
Ernesto <ernestosanchezinfante@gmail.com>
2022-11-20 18:28:53 -0600

    codigo base

--- models/server.js
+++ models/server.js
@@ -4,6 +4,7 @@ const http = require('http');
 const socketio = require('socket.io');
 const path = require('path');
 const Sockets = require('./sockets');
+const cors = require('cors');
 
 //servidor de sockets
 const server = require('http').createServer(this.app);
@@ -24,6 +25,9 @@ class Server {
     middlewares(){
         //Desplegar el directorio publico
         this.app.use( express.static( path.resolve( __dirname, '../public' ) ));
+
+        //Cors
+        this.app.use( cors() );
     }
 
     configurarSockets(){
